3b974e3a4365fb476f90d51de972a24915bf64a1
Michael <michaelwilliamtang@gmail.com>
2020-06-09 12:11:14 -0700

    Added log-unbatch versions, updated original versions of graphing scripts

--- Metaphlan_6-9-20.R
+++ Metaphlan_6-9-20.R
@@ -2,12 +2,18 @@ library(tidyverse)
 data_dir <- "Data"
 graph_dir <- "Graphs/Metaphlan"
 summarize <- dplyr::summarize
+desc <- "Log_Unbatch"
 
 # read metaphlan
-metaphlan_df <- read.table(file.path(data_dir, "hf-metaphlan-final-combat.pcl"), sep = "\t", stringsAsFactors = FALSE, row.names = 1)
+metaphlan_df <- read.table(file.path(data_dir, "metaphlan2_april_30.csv"), sep = ",", stringsAsFactors = FALSE)
+metaphlan_df$V1[50] <- paste(metaphlan_df$V1[50], "b", sep = "_") # dup row
+rownames(metaphlan_df) <- metaphlan_df$V1
 metaphlan_df <- metaphlan_df %>%
+  select(-V1) %>%
   as.matrix() %>% t() %>%
   as_tibble()
+metaphlan_df$participant_id <- metaphlan_df$study_id %>% str_sub(1,6)
+# metaphlan_df %>% select(study_id, participant_id) %>% View()
 # View(metaphlan_df[,1:100])
 
 # get top 5 phyla
@@ -16,11 +22,12 @@ met_phyla_df <- metaphlan_df %>% filter(omni_comparison == "comparison") %>%
   select(participant_id, collection_type, phyla)
 colnames(met_phyla_df) <- met_phyla_df %>% colnames() %>%
   str_replace(".*p__", "")
-met_phyla_df$participant_id[16] <- paste(met_phyla_df$participant_id[16], "b", sep = "") # anomaly participant (dup)
-met_phyla_df$participant_id[18] <- paste(met_phyla_df$participant_id[18], "b", sep = "")
+met_phyla_df$participant_id[16] <- paste(met_phyla_df$participant_id[16], "b", sep = "_") # anomaly participant (dup)
+met_phyla_df$participant_id[18] <- paste(met_phyla_df$participant_id[18], "b", sep = "_")
 met_phyla_df <- met_phyla_df %>%
   gather("taxa", "val", -c(participant_id, collection_type))
 met_phyla_df$val <- as.numeric(met_phyla_df$val)
+met_phyla_df$val <- log2(met_phyla_df$val + 1) # allows for 0's
 phy <- met_phyla_df %>%
   group_by(taxa) %>%
   summarize(mean_val = mean(val)) %>%
@@ -37,7 +44,7 @@ comp_phyla <- met_phyla_df %>%
   arrange(desc(collection_diff_abs))
 diff_20_phy <- comp_phyla$taxa[1:20]
 comp_phyla <- comp_phyla %>% filter(taxa %in% diff_20_phy)
-pdf(file.path(graph_dir, "Phyla_By_Collection_Diff.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Phyla_By_Collection_Diff.pdf", sep = "_")), width = 6, height = 4)
 comp_phyla %>% ggplot(aes(x = reorder(taxa, -collection_diff), y = collection_diff)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Phylum") +
@@ -52,14 +59,14 @@ met_phyla_df2 <- met_phyla_df %>%
   summarize(mean_val = mean(val)) %>%
   arrange(mean_val)
 # View(met_phyla_df)
-pdf(file.path(graph_dir, "Top_5_Phyla_Side.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Top_5_Phyla_Side.pdf", sep = "_")), width = 6, height = 4)
 met_phyla_df2 %>% ggplot(aes(x = reorder(taxa, -mean_val), y = mean_val, fill = collection_type)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Phylum") +
   ylab("Relative Abundance") +
   theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1))
 dev.off()
-pdf(file.path(graph_dir, "Top_5_Phyla_Stack.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Top_5_Phyla_Stack.pdf", sep = "_")), width = 6, height = 4)
 met_phyla_df2 %>% ggplot(aes(x = reorder(taxa, -mean_val), y = mean_val, fill = collection_type)) +
   geom_bar(position = "stack", stat = "identity") +
   xlab("Phylum") +
@@ -71,7 +78,7 @@ met_phyla_df2 <- met_phyla_df %>%
   group_by(collection_type) %>%
   summarize(mean_val = mean(val)) %>%
   arrange(mean_val)
-pdf(file.path(graph_dir, "Phyla_Aggregated.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Phyla_Aggregated.pdf", sep = "_")), width = 6, height = 4)
 met_phyla_df2 %>% ggplot(aes(x = reorder(collection_type, -mean_val), y = mean_val)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Collection Type") +
@@ -85,11 +92,12 @@ met_genera_df <- metaphlan_df %>% filter(omni_comparison == "comparison") %>%
   select(participant_id, collection_type, genera)
 colnames(met_genera_df) <- met_genera_df %>% colnames() %>%
   str_replace(".*g__", "")
-met_genera_df$participant_id[16] <- paste(met_genera_df$participant_id[16], "b", sep = "") # anomaly participant (dup)
-met_genera_df$participant_id[18] <- paste(met_genera_df$participant_id[18], "b", sep = "")
+met_genera_df$participant_id[16] <- paste(met_genera_df$participant_id[16], "b", sep = "_") # anomaly participant (dup)
+met_genera_df$participant_id[18] <- paste(met_genera_df$participant_id[18], "b", sep = "_")
 met_genera_df <- met_genera_df %>%
   gather("taxa", "val", -c(participant_id, collection_type))
 met_genera_df$val <- as.numeric(met_genera_df$val)
+met_genera_df$val <- log2(met_genera_df$val + 1) # allows for 0's
 gen <- met_genera_df %>%
   group_by(taxa) %>%
   summarize(mean_val = mean(val)) %>%
@@ -106,7 +114,7 @@ comp_genera <- met_genera_df %>%
   arrange(desc(collection_diff_abs))
 diff_20_gen <- comp_genera$taxa[1:20]
 comp_genera <- comp_genera %>% filter(taxa %in% diff_20_gen)
-pdf(file.path(graph_dir, "Genera_By_Collection_Diff.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Genera_By_Collection_Diff.pdf", sep = "_")), width = 6, height = 4)
 comp_genera %>% ggplot(aes(x = reorder(taxa, -collection_diff), y = collection_diff)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Genus") +
@@ -121,14 +129,14 @@ met_genera_df2 <- met_genera_df %>%
   summarize(mean_val = mean(val)) %>%
   arrange(mean_val)
 # View(met_genera_df)
-pdf(file.path(graph_dir, "Top_20_Genera_Side.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Top_20_Genera_Side.pdf", sep = "_")), width = 6, height = 4)
 met_genera_df2 %>% ggplot(aes(x = reorder(taxa, -mean_val), y = mean_val, fill = collection_type)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Genus") +
   ylab("Relative Abundance") +
   theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1))
 dev.off()
-pdf(file.path(graph_dir, "Top_20_Genera_Stack.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Top_20_Genera_Stack.pdf", sep = "_")), width = 6, height = 4)
 met_genera_df2 %>% ggplot(aes(x = reorder(taxa, -mean_val), y = mean_val, fill = collection_type)) +
   geom_bar(position = "stack", stat = "identity") +
   xlab("Genus") +
@@ -140,7 +148,7 @@ met_genera_df2 <- met_genera_df %>%
   group_by(collection_type) %>%
   summarize(mean_val = mean(val)) %>%
   arrange(mean_val)
-pdf(file.path(graph_dir, "Genera_Aggregated.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Genera_Aggregated.pdf", sep = "_")), width = 6, height = 4)
 met_genera_df2 %>% ggplot(aes(x = reorder(collection_type, -mean_val), y = mean_val)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Collection Type") +
--- PCL_6-9-20.R
+++ PCL_6-9-20.R
@@ -2,24 +2,36 @@ library(tidyverse)
 data_dir <- "Data"
 graph_dir <- "Graphs/PCL"
 summarize <- dplyr::summarize
+desc <- "Log_Unbatch"
 
 # read pcl
-pcl_df <- read.table(file.path(data_dir, "hf-abundance-final-combat.pcl"), sep = "\t", stringsAsFactors = FALSE, row.names = 1)
-pcl_df <- pcl_df %>% 
-  as.matrix() %>% t() %>% 
+pcl_df <- read.table(file.path(data_dir, "pathabundance_april_30.csv"), sep = ",", stringsAsFactors = FALSE)
+pcl_df$V1[50] <- paste(pcl_df$V1[50], "b", sep = "_") # dup row
+full_names <- pcl_df$V1
+names(full_names) = full_names
+full_names[184:length(pcl_df$V1)] = 184:length(pcl_df$V1)
+rownames(pcl_df) <- full_names
+pcl_df <- pcl_df %>%
+  select(-V1) %>%
+  as.matrix() %>% t() %>%
   as_tibble()
-# View(pcl_df[,1:50])
+pcl_df$participant_id <- pcl_df$study_id %>% str_sub(1,6)
+# pcl_df %>% select(study_id, participant_id) %>% View()
+# View(pcl_df[,c(1, 1859)])
+# View(pcl_df[,1:100])
 
 # get top 40 pathways
-pathways <- grep("\\.\\.", colnames(pcl_df), perl = T)
+pathways <- grep(": ", names(full_names), perl = T)
 pcl_pathways_df <- pcl_df %>% filter(omni_comparison == "comparison") %>%
   select(participant_id, collection_type, pathways)
-pcl_pathways_df$participant_id[16] <- paste(pcl_pathways_df$participant_id[16], "b", sep = "") # anomaly participant (dup)
-pcl_pathways_df$participant_id[18] <- paste(pcl_pathways_df$participant_id[18], "b", sep = "")
+pcl_pathways_df$participant_id[16] <- paste(pcl_pathways_df$participant_id[16], "b", sep = "_") # anomaly participant (dup)
+pcl_pathways_df$participant_id[18] <- paste(pcl_pathways_df$participant_id[18], "b", sep = "_")
 pcl_pathways_df <- pcl_pathways_df %>%
   gather("pathway", "val", -c(participant_id, collection_type))
 pcl_pathways_df$val <- as.numeric(pcl_pathways_df$val)
+pcl_pathways_df$val <- log2(pcl_pathways_df$val + 1) # allows for 0's
 path <- pcl_pathways_df %>%
+  mutate(pathway = full_names[pathway]) %>%
   group_by(pathway) %>%
   summarize(mean_val = mean(val)) %>%
   arrange(desc(mean_val))
@@ -35,7 +47,7 @@ comp_pathways <- pcl_pathways_df %>%
   arrange(desc(collection_diff_abs))
 diff_20_path <- comp_pathways$pathway[1:20]
 comp_pathways <- comp_pathways %>% filter(pathway %in% diff_20_path)
-pdf(file.path(graph_dir, "Pathways_By_Collection_Diff.pdf"), width = 18, height = 12)
+pdf(file.path(graph_dir, paste(desc, "Pathways_By_Collection_Diff.pdf", sep = "_")), width = 18, height = 12)
 comp_pathways %>% ggplot(aes(x = reorder(pathway, -collection_diff), y = collection_diff)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Pathway") +
@@ -45,19 +57,19 @@ dev.off()
 
 # plot top pathways
 pcl_pathways_df2 <- pcl_pathways_df %>%
-  filter(pathway %in% top_40_path) %>%
+  filter(pathway %in% top_12_path) %>%
   group_by(pathway, collection_type) %>%
   summarize(mean_val = mean(val)) %>%
   arrange(mean_val)
 # View(pcl_pathways_df)
-pdf(file.path(graph_dir, "Top_40_Pathways_Side.pdf"), width = 18, height = 12)
+pdf(file.path(graph_dir, paste(desc, "Top_40_Pathways_Side.pdf", sep = "_")), width = 18, height = 12)
 pcl_pathways_df2 %>% ggplot(aes(x = reorder(pathway, -mean_val), y = mean_val, fill = collection_type)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Pathway") +
   ylab("Relative Abundance") +
   theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1))
 dev.off()
-pdf(file.path(graph_dir, "Top_40_Pathways_Stack.pdf"), width = 18, height = 12)
+pdf(file.path(graph_dir, paste(desc, "Top_40_Pathways_Stack.pdf", sep = "_")), width = 18, height = 12)
 pcl_pathways_df2 %>% ggplot(aes(x = reorder(pathway, -mean_val), y = mean_val, fill = collection_type)) +
   geom_bar(position = "stack", stat = "identity") +
   xlab("Pathway") +
@@ -65,11 +77,11 @@ pcl_pathways_df2 %>% ggplot(aes(x = reorder(pathway, -mean_val), y = mean_val, f
   theme(axis.text.x = element_text(size = 10, angle = 45, hjust = 1))
 dev.off()
 pcl_pathways_df2 <- pcl_pathways_df %>%
-  filter(pathway %in% top_40_path) %>%
+  filter(pathway %in% top_12_path) %>%
   group_by(collection_type) %>%
   summarize(mean_val = mean(val)) %>%
   arrange(mean_val)
-pdf(file.path(graph_dir, "Pathways_Aggregated.pdf"), width = 6, height = 4)
+pdf(file.path(graph_dir, paste(desc, "Pathways_Aggregated.pdf", sep = "_")), width = 6, height = 4)
 pcl_pathways_df2 %>% ggplot(aes(x = reorder(collection_type, -mean_val), y = mean_val)) +
   geom_bar(position = "dodge", stat = "identity") +
   xlab("Collection Type") +
