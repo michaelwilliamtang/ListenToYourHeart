6815e45331f9e81795399439ed77388dc38b3c15
Michael <michaelwilliamtang@gmail.com>
2020-06-11 16:23:15 -0700

    Improved check metaphlan with additional, more detailed, txt and added check pcl

--- Check_Metaphlan.R
+++ Check_Metaphlan.R
@@ -1,21 +1,19 @@
 # 6/11/20
-# Checks summation of unlogged metaphlan values
+# Checks summation of unlogged metaphlan values and graphs distributions, per level
 
 library(tidyverse)
 data_dir <- "Data/Tidy"
-graph_dir <- "Graphs/Metaphlan/Check_Distribution_Scaled"
-if (!dir.exists(graph_dir)) dir.create(graph_dir)
 summarize <- dplyr::summarize
 ds1 <- "metaphlan"
 
-load(file.path(data_dir, paste("Tidy_Scaled_", ds1, ".RData", sep = "")))
-metaphlan_df <- met_taxa_df
-
-check_distribution <- function(met_lev, met_reg, met_lab, is_batch = F, coll_type = "omni") {
+check_distribution <- function(logged = F, met_lev, met_reg, met_lab, is_batch = F, coll_type = "omni") {
   print(met_lev)
   
-  graph_dir2 <- graph_dir
-  # if (!dir.exists(graph_dir2)) dir.create(graph_dir2)
+  graph_dir2 <- "Graphs/Metaphlan/Check_Distribution"
+  if (logged) graph_dir2 <- paste(graph_dir2, "Log", sep = "_")
+  if (!dir.exists(graph_dir2)) dir.create(graph_dir2)
+  load(file.path(data_dir, paste("Tidy_", ds1, ".RData", sep = "")))
+  metaphlan_df <- met_taxa_df
   
   if (!is.na(is_batch)) metaphlan_df <- metaphlan_df %>%
     filter(batch == is_batch)
@@ -30,12 +28,19 @@ check_distribution <- function(met_lev, met_reg, met_lab, is_batch = F, coll_typ
   met_phyla_df$batched[which(met_phyla_df$batch)] <- "batched"
   
   # unlog
-  met_phyla_df$val <- 2 ^ met_phyla_df$val - 1
+  # met_phyla_df$val <- 2 ^ met_phyla_df$val - 1
   
   check_df <- met_phyla_df %>% 
     group_by(batched, collection_type, study_id) %>%
-    summarize(total = sum(val))
+    summarize(total = sum(val)) %>%
+    arrange(desc(total))
   write_tsv(check_df, file.path(graph_dir2, paste(met_lab, "Totals.txt", sep = "_")))
+  
+  check_df <- met_phyla_df %>% 
+    group_by(batched, collection_type, study_id, taxa) %>%
+    summarize(total = sum(val)) %>%
+    arrange(desc(total))
+  write_tsv(check_df, file.path(graph_dir2, paste(met_lab, "Taxa_Totals.txt", sep = "_")))
   
   pdf(file.path(graph_dir2, paste(met_lab, "Check_Distribution.pdf", sep = "_")), width = 18, height = 12)
   print(met_phyla_df %>% ggplot(aes(x = val, fill = taxa)) +
@@ -51,6 +56,6 @@ names(met_regs) <- met_levs
 ### comparing for metaphlan, phyla
 
 for (i in 1:length(met_levs)) {
-  check_distribution(met_lev = met_levs[i], met_reg = met_regs[i], met_lab = met_labs[i],
+  check_distribution(logged = F, met_lev = met_levs[i], met_reg = met_regs[i], met_lab = met_labs[i],
                      is_batch = F, coll_type = "omni")
 }
---
+++ Check_PCL.R
@@ -0,0 +1,49 @@
+# 6/11/20
+# Checks summation of unlogged pcl values and graphs distributions
+
+library(tidyverse)
+data_dir <- "Data/Tidy"
+summarize <- dplyr::summarize
+ds1 <- "pcl"
+
+check_distribution <- function(logged = F, is_batch = F, coll_type = "omni") {
+  
+  graph_dir2 <- "Graphs/PCL/Check_Distribution"
+  if (logged) graph_dir2 <- paste(graph_dir2, "Log", sep = "_")
+  if (!dir.exists(graph_dir2)) dir.create(graph_dir2)
+  load(file.path(data_dir, paste("Tidy_", ds1, ".RData", sep = "")))
+  pcl_df <- pcl_pathway_df
+  
+  if (!is.na(is_batch)) pcl_df <- pcl_df %>%
+    filter(batch == is_batch)
+  if (!is.na(coll_type)) pcl_df <- pcl_df %>%
+    filter(collection_type == coll_type)
+  
+  pcl_pathway_df <- pcl_df %>%
+    filter(omni_comparison == "comparison")
+  pcl_pathway_df$batched <- "unbatched"
+  pcl_pathway_df$batched[which(pcl_pathway_df$batch)] <- "batched"
+  
+  # unlog
+  # pcl_pathway_df$val <- 2 ^ pcl_pathway_df$val - 1
+  
+  check_df <- pcl_pathway_df %>% 
+    group_by(batched, collection_type, study_id) %>%
+    summarize(total = sum(val)) %>%
+    arrange(desc(total))
+  write_tsv(check_df, file.path(graph_dir2, paste("Totals.txt", sep = "_")))
+  
+  check_df <- pcl_pathway_df %>% 
+    group_by(batched, collection_type, study_id, pathway) %>%
+    summarize(total = sum(val)) %>%
+    arrange(desc(total))
+  write_tsv(check_df, file.path(graph_dir2, paste("Pathway_Totals.txt", sep = "_")))
+  
+  pdf(file.path(graph_dir2, paste("Check_Distribution.pdf", sep = "_")), width = 18, height = 12)
+  print(pcl_pathway_df %>% ggplot(aes(x = val, fill = pathway)) +
+    geom_histogram(binwidth = 0.1, alpha = 0.5, position = "identity") +
+    theme(legend.position = "none"))
+  dev.off()
+}
+
+check_distribution(logged = F, is_batch = F, coll_type = "omni")
